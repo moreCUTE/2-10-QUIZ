b4d9f6e10b056f28d5c198d2b826512a086d5d81
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2023-02-10 14:01:46 -0700

    Create if-statements.cpp

---
+++ if-statements.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	char input;
+
+	cout << "what fish yo favotite" << endl; 
+	cin >> input;
+	if (input == 'g')
+		cout << "goldfish" << endl;
+	else if (input == 'b')
+		cout << "bass" << endl;
+	else if (input == 's')
+		cout << "shark" << endl;
+	else if (input == 'c')
+		cout << "cod sandwitch" << endl;
+	else
+		cout << "blue whale" << endl;
+			
+			
+
+}
