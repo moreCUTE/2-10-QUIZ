519724f72a538af4dc31349343d95a31101d2804
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2023-02-10 13:24:44 -0700

    Create while-loops.cpp

---
+++ while-loops.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+//WHILE LOOPS ---------------------------------------------------------
+	int x = 3;
+		while(x < 33) {
+		cout << x << " ";
+		x += 5;
+	}
+
+		cout << endl;
+		cout << "-----------------------------------------" << endl;
+		
+
+		char input;
+		cout << "DOUGHNTJT" << endl;
+		cout << "u wqant another donut" << endl;
+		cin >> input;
+
+		while (input == 'y') {
+			cout << "dodunut" << endl;
+			cout << "you want another donut" << endl;
+			cin >> input;
+
+		}
+		cout << "alr goodbye :(" << endl;
+			
+			
+
+}
